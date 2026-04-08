a21b7f4e75b8d3a634f386d3746e7e08c99d16c6
rozoomcool <rozoom.um@gmail.com>
2026-04-08 21:12:54 +0300

    add markDone

---
+++ cmd/markDone.go
@@ -0,0 +1,58 @@
+/*
+Copyright © 2026 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/rozoomcool/task-cli/internal/model"
+	"github.com/rozoomcool/task-cli/internal/service"
+	"github.com/spf13/cobra"
+)
+
+// markDoneCmd represents the markDone command
+var markDoneCmd = &cobra.Command{
+	Use:   "mark-done",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) > 0 {
+			id, err := strconv.Atoi(args[0])
+			if err != nil {
+				fmt.Println("Required parameters are not provided")
+				return
+			}
+
+			taskService := service.NewTaskService()
+			err = taskService.UpdatedTask(id, "", model.TaskStatusDone)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			fmt.Println("Task successfully updated")
+		} else {
+			fmt.Println("Required parameters are not provided")
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(markDoneCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// markDoneCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// markDoneCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
